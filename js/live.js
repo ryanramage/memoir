5295632e917c0b64e3f47048c831212254007b48
Ryan Ramage <ryan.ramage@gmail.com>
2013-05-23 14:11:23 -0600

    Add changes feed if available

--- js/live.js
+++ js/live.js
@@ -3,19 +3,37 @@
 
     var currentETag;
 
-    var fetchStatus = function(callback) {
+    onLoad(function pageLoaded() {
+        console.log('starting live reload');
+        if (window.EventSource) return eventsource();
+        else setInterval(polling, 1500);
+    });
 
-        var client = new XMLHttpRequest();
-        client.onreadystatechange = function() {
-        // in case of network errors this might not give reliable results
-        if(this.readyState == this.DONE)
-          callback(null, client.getResponseHeader('ETag').toString());
-        }
-        client.open("HEAD", '_ddoc');
-        client.send();
+
+    /* Use couchdb eventsource changes feed. Reload page when id matches this ddoc */
+    function eventsource() {
+
+        fetchName(function(err, name){
+            var ddoc_name = '_design/' + name;
+
+            var source = new EventSource("_db/_changes?feed=eventsource&since=now");
+            var sourceListener = function(e) {
+                var data = JSON.parse(e.data);
+
+                if (data.id !== ddoc_name) return;
+
+
+                window.location.reload();
+            };
+            // start listening for events
+            source.addEventListener('message', sourceListener , false);
+
+        });
     }
 
-    var checkToReload = function() {
+
+    /* Poll for etag changes to the ddoc */
+    function polling() {
         fetchStatus(function(err, etag){
             if (!currentETag) currentETag = etag;
             else {
@@ -27,37 +45,59 @@
     }
 
 
-    var pageLoaded = function() {
-        console.log('starting live reload');
-        setInterval(checkToReload, 1500);
+    function fetchStatus(callback) {
+
+        var client = new XMLHttpRequest();
+        client.onreadystatechange = function() {
+            if (this.readyState == this.DONE) {
+                callback(null, client.getResponseHeader('ETag').toString());
+            }
+        };
+        client.open("HEAD", '_ddoc');
+        client.send();
     }
 
+    function fetchName(callback) {
+        var client = new XMLHttpRequest();
+        client.onreadystatechange = function() {
+            if (this.readyState == this.DONE) {
+                var data = JSON.parse(this.response);
+                callback(null, data.name);
+            }
+        };
+        client.open("GET", '_ddoc/_info');
+        client.send();
+    }
 
-    if (document.addEventListener) {
-        //Standards. Hooray! Assumption here that if standards based,
-        //it knows about DOMContentLoaded.
-        document.addEventListener("DOMContentLoaded", pageLoaded, false);
-        window.addEventListener("load", pageLoaded, false);
-    } else if (window.attachEvent) {
-        window.attachEvent("onload", pageLoaded);
+    function onLoad(callback) {
 
-        testDiv = document.createElement('div');
-        try {
-            isTop = window.frameElement === null;
-        } catch (e) {}
+        if (document.addEventListener) {
+            //Standards. Hooray! Assumption here that if standards based,
+            //it knows about DOMContentLoaded.
+            document.addEventListener("DOMContentLoaded", callback, false);
+            window.addEventListener("load", callback, false);
+        } else if (window.attachEvent) {
+            window.attachEvent("onload", callback);
 
-        //DOMContentLoaded approximation that uses a doScroll, as found by
-        //Diego Perini: http://javascript.nwbox.com/IEContentLoaded/,
-        //but modified by other contributors, including jdalton
-        if (testDiv.doScroll && isTop && window.external) {
-            scrollIntervalId = setInterval(function () {
-                try {
-                    testDiv.doScroll();
-                    pageLoaded();
-                } catch (e) {}
-            }, 30);
+            testDiv = document.createElement('div');
+            try {
+                isTop = window.frameElement === null;
+            } catch (e) {}
+
+            //DOMContentLoaded approximation that uses a doScroll, as found by
+            //Diego Perini: http://javascript.nwbox.com/IEContentLoaded/,
+            //but modified by other contributors, including jdalton
+            if (testDiv.doScroll && isTop && window.external) {
+                scrollIntervalId = setInterval(function () {
+                    try {
+                        testDiv.doScroll();
+                        callback();
+                    } catch (e) {}
+                }, 30);
+            }
         }
     }
+
 
 
 
